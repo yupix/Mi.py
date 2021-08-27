04856f2dc6b70f5b46078a641cbc5f2127fe1ff4
yupix <yupi0982@outlook.jp>
2021-08-27 16:32:22 +0900

    feat: setup.pyを追加

--- misskey/__init__.py
+++ misskey/__init__.py
@@ -1,3 +1,21 @@
 from misskey.reaction import Reaction
 
-__all__ = ['Reaction']
+__title__ = 'misskey'
+__author__ = 'yupix'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021-present yupix'
+__author_email__ = 'yupi0982@outlook.jp'
+__url__ = 'https://github.com/yupix/Mi.py'
+__version__ = '0.0.1a'
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+from .bot import *
+from context import *
+from http import *
+from instance import *
+from message import *
+from note import *
+from reaction import *
+from router import *
+from user import *
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+import pathlib
+
+
+description = 'A Python wrapper for the Misskey API'
+readme_file = pathlib.Path(__file__).parent/'README.md'
+with readme_file.open(encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='mi.py',
+    version='0.0.1a',
+    install_requires=['aiohttp', 'websockets'],
+    url='https://github.com/yupix/alfakana',
+    author='yupix',
+    author_email='yupi0982@outlook.jp',
+    license='MIT',
+    python_requires='>=3.8, <4.0',
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3.9.6',
+        'Natural Language :: Japanese',
+        'License :: OSI Approved :: MIT License',
+    ]
+)
