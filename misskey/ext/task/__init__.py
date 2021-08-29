7f10942eae0470c4e8bec52571233915cac442bb
yupix <yupi0982@outlook.jp>
2021-08-29 09:54:58 +0900

    feat: taskにstopメソッドを追加

--- misskey/ext/task/__init__.py
+++ misskey/ext/task/__init__.py
@@ -5,13 +5,22 @@ class Loop(object):
     def __init__(self, func, seconds=None):
         self.seconds = seconds
         self.func = func
+        self._task = None
+        self.stop_next_iteration = None
 
     def start(self, *args, **kwargs):
         _loop = asyncio.get_event_loop()
-        return _loop.create_task(self.task(*args, **kwargs))
+        self._task = _loop.create_task(self.task(*args, **kwargs))
+        return self._task
+
+    def stop(self):
+        if not self._task.done():
+            self.stop_next_iteration = True
 
     async def task(self, *args, **kwargs):
         while True:
+            if self.stop_next_iteration:
+                return
             await self.func(self.seconds, *args, **kwargs)
             await asyncio.sleep(self.seconds)
 
