48cde71fb9fd5838da41800e21ca1e02486ee033
yupix <yupi0982@outlook.jp>
2021-08-29 10:46:42 +0900

    feat: __init__.pyの__all__に存在しないクラス等を追加

--- misskey/__init__.py
+++ misskey/__init__.py
@@ -8,14 +8,14 @@ __version__ = '0.0.2'
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
-from .bot import *
+from .bot import Bot, BotBase
 from .ext import task
-from .ext.task import *
-from .http import *
-from .note import *
-from .router import *
-from .utils import *
-from .drive import *
+from .ext.task import Loop, loop
+from .http import WebSocket
+from .note import Message, Header, Note, Reaction, ReactionNote, User, Instance
+from .router import Router
+from .utils import bool_to_string
+from .drive import Properties, Drive
 
 __all__ = [
     'bot',
@@ -24,9 +24,14 @@ __all__ = [
     'router',
     'ext',
     'task',
+    'Loop',
+    'loop',
+    'WebSocket',
     'utils',
     'drive',
     'Note',
+    'ReactionNote',
+    'Instance',
     'bool_to_string',
     'Message',
     'Header',
@@ -34,5 +39,6 @@ __all__ = [
     'Reaction',
     'Bot',
     'Router',
-    'Drive'
+    'Drive',
+    'Properties'
 ]
