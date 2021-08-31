e691f0cb271fe1813e3c77866bd5781881e4cb4e
yupix <yupi0982@outlook.jp>
2021-09-01 00:03:50 +0900

    feat: bump version 0.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with readme_file.open(encoding='utf-8') as fh:
 
 setup(
     name='mi.py',
-    version='0.0.2',
+    version='0.1.0',
     install_requires=['websockets', 'requests'],
     url='https://github.com/yupix/mi.py',
     author='yupix',
