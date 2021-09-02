e2d30163092f8f14653160ce04ae57421e55555e
yupix <yupi0982@outlook.jp>
2021-09-02 12:40:53 +0900

    fix: インデントが潰されてた

--- mi/note.py
+++ mi/note.py
@@ -149,11 +149,10 @@ class Instance(object):
         'theme_color'
     )
 
-
-def __init__(self, data):
-    self.home = data.get('instance', {}).get('home')
-    self.name = data.get('instance', {}).get('name')
-    self.software_name = data.get('instance', {}).get('softwareName')
-    self.icon_url = data.get('instance', {}).get('iconUrl')
-    self.favicon_url = data.get('instance', {}).get('faviconUrl')
-    self.theme_color = data.get('instance', {}).get('themeColor')
+    def __init__(self, data):
+        self.home = data.get('instance', {}).get('home')
+        self.name = data.get('instance', {}).get('name')
+        self.software_name = data.get('instance', {}).get('softwareName')
+        self.icon_url = data.get('instance', {}).get('iconUrl')
+        self.favicon_url = data.get('instance', {}).get('faviconUrl')
+        self.theme_color = data.get('instance', {}).get('themeColor')
