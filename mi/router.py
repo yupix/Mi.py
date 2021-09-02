9ae0dfaadd22c9bff398c1d79f1f54e65754f137
yupix <yupi0982@outlook.jp>
2021-09-02 13:51:57 +0900

    chore: RouterクラスにDocStringを追加

--- mi/router.py
+++ mi/router.py
@@ -1,13 +1,34 @@
+"""
+Misskeyのチャンネルへの接続や、メッセージのキャプチャ等のWebSocket関連
+"""
+
 import json
 
 from mi import Message
 
 
-class Router(object):
-    def __init__(self, ws):
-        self.ws = ws
+class Router:
+    """
+    Attributes
+    ----------
+    web_socket : Any
+        WebSocketクライアント
+    """
 
-    async def channels(self, channel_list: list):
+    def __init__(self, web_socket):
+        self.web_socket = web_socket
+
+    async def channels(self, channel_list: list) -> None:
+        """
+        与えられたlistを元にチャンネルに接続します
+        Parameters
+        ----------
+        channel_list : List[str]
+
+        Returns
+        -------
+        None: None
+        """
         channel_dict = {'global': self.global_time_line, 'main': self.main_channel,
                         'home': self.home_time_line, 'local': self.local_time_line}
         try:
@@ -17,8 +38,15 @@ class Router(object):
         except KeyError:
             pass
 
-    async def global_time_line(self):
-        await self.ws.send(json.dumps({
+    async def global_time_line(self) -> None:
+        """
+        WebSocketでGlobalTimeLineに接続します
+
+        Returns
+        -------
+        None: None
+        """
+        await self.web_socket.send(json.dumps({
             'type': 'connect',
             'body': {
                 'channel': 'globalTimeline',
@@ -29,8 +57,14 @@ class Router(object):
             }
         }, ensure_ascii=False))
 
-    async def main_channel(self):
-        await self.ws.send(json.dumps({
+    async def main_channel(self) -> None:
+        """
+        WebSocketでMainチャンネルに接続します
+        Returns
+        -------
+        None: None
+        """
+        await self.web_socket.send(json.dumps({
             'type': 'connect',
             'body': {
                 'channel': 'main',
@@ -41,8 +75,14 @@ class Router(object):
             }
         }, ensure_ascii=False))
 
-    async def home_time_line(self):
-        await self.ws.send(json.dumps({
+    async def home_time_line(self) -> None:
+        """
+        WebSocketでHomeTimeLineに接続します
+        Returns
+        -------
+        None: None
+        """
+        await self.web_socket.send(json.dumps({
             'type': 'connect',
             'body': {
                 'channel': 'homeTimeline',
@@ -53,8 +93,14 @@ class Router(object):
             }
         }, ensure_ascii=False))
 
-    async def local_time_line(self):
-        await self.ws.send(json.dumps({
+    async def local_time_line(self) -> None:
+        """
+        WebSocketでLocalTimeLineに接続します
+        Returns
+        -------
+        None: None
+        """
+        await self.web_socket.send(json.dumps({
             'type': 'connect',
             'body': {
                 'channel': 'localTimeline',
@@ -65,9 +111,20 @@ class Router(object):
             }
         }, ensure_ascii=False))
 
-    async def capture_message(self, message: Message):
+    async def capture_message(self, message: Message) -> None:
+        """
+        与えられたメッセージを元にnote idを取得し、そのメッセージをon_message等の監視対象に追加します
+
+        Parameters
+        ----------
+        message : Message
+
+        Returns
+        -------
+
+        """
         if hasattr(message, 'id'):
-            await self.ws.send(json.dumps({
+            await self.web_socket.send(json.dumps({
                 'type': 'subNote',
                 'body': {
                     'id': f'{message.note.id}'
