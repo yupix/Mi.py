978bf7254488c574899f473feb3f4b8385bfa6c8
yupix <yupi0982@outlook.jp>
2021-09-03 10:34:09 +0900

    feat: Noteに関連するクラスの引数をdictから詳細なものに変更

--- mi/note.py
+++ mi/note.py
@@ -1,23 +1,39 @@
 import json
 import re
-from typing import Any
-
+from typing import Any, Final
 import requests
+
+from mi import Drive
+from mi.utils import upper_to_lower
+
+
+class _EmptyNote:
+    def __bool__(self) -> bool:
+        return False
+
+    def __repr__(self) -> str:
+        return 'Note.Empty'
+
+    def __len__(self) -> int:
+        return 0
+
+
+EmptyEmbed: Final = _EmptyNote()
 
 
 class Message(object):
-    def __init__(self, data: Any, ws=None):
+    def __init__(self, data: Any, web_socket=None):
         data = json.loads(data)
         self.type = data.get('type')
         self.header = Header(data.get('body', {}))
         if note := data.get('body', {}).get('body', None):
-            self.note = Note(note, ws)
+            self.note = Note(**upper_to_lower(note))
         elif note := data.get('createdNote', None):  # APIの場合
-            self.note = Note(note, ws)
+            self.note = Note(**upper_to_lower(note))
         else:
             data = data.get('body', {}).get('res', {}).get('createdNote', {})  # WebSocketsの場合
             data['res'] = True
-            self.note = Note(data, ws)
+            self.note = Note(data)
 
 
 class Header(object):
@@ -27,20 +43,60 @@ class Header(object):
 
 
 class Note(object):
-    def __init__(self, data=None, ws=None, text: str = None, cw: str = None, via_mobile: bool = False, token: str = None,
+    def __init__(self,
+                 id_: str = None,
+                 created_at: str = None,
+                 user_id: str = None,
+                 author=None,
+                 text: str = None,
+                 cw: str = None,
+                 visibility=None,
+                 renote_count=None,
+                 replies_count=None,
+                 reactions=None,
+                 emojis=None,
+                 file_ids=None,
+                 files=None,
+                 reply_id=None,
+                 renote_id=None,
+                 via_mobile=None,
+                 poll=None,
+                 res=None,
+                 data=None,
+                 token: str = None,
                  origin_uri: str = None):
         if data is None:  # リストをデフォルトにすると使いまわされて良くないので毎回初期化する必要がある。
             data = {}
+        self.id = id_
+        self.created_at = created_at
+        self.user_id = user_id
+        self.author = User(**upper_to_lower(author))
+        self.text = text
+        self.visibility = visibility
+        self.renote_count = renote_count
+        self.replies_count = replies_count
+        self.reactions = reactions
+        self.emojis = emojis
+        self.file_ids = file_ids
+        self.files = files
+        self.reply_id = reply_id
+        self.renote_id = renote_id
+        self.poll = poll
+        self.res = res
         self.token = token
-        self.ws = ws
         self.field = data
         self.origin_uri = origin_uri
         if not data:  # 型変更としてではなく、投稿などに使う際に必要
             self.field['text'] = text
-            if len(cw) != 0:
+            if cw:
                 self.field['cw'] = cw
             self.field['viaMobile'] = via_mobile
             self.field['i'] = token
+
+    @classmethod
+    def from_dict(cls, data):
+
+        self = cls.__new__(cls)
 
         after_key = {'user': 'author'}
         for attr in ('id', 'createdAt', 'userId', 'user', 'text', 'cw',
@@ -78,7 +134,6 @@ class Note(object):
         -------
         self: Note
         """
-        from mi import Drive
         self.field['fileIds'] = []
         if file_id is None:
             res = Drive(token=self.token, origin_uri=self.origin_uri).upload(path=path, name=name)
@@ -154,10 +209,36 @@ class User(object):
         'avatar_blurhash',
         'avatar_color',
         'instance',
-        'emojis'
+        'emojis',
+        'is_admin'
     )
 
-    def __init__(self, data: dict):
+    def __init__(self,
+                 id_: str = None,
+                 name: str = None,
+                 username: str = None,
+                 host: str = None,
+                 avatar_url: str = None,
+                 avatar_blurhash: str = None,
+                 avatar_color: str = None,
+                 instance: dict = None,
+                 emojis=None,
+                 is_admin=None
+                 ):
+        self.id = id_
+        self.name = name
+        self.username = username
+        self.host = host
+        self.avatar_url = avatar_url
+        self.avatar_blurhash = avatar_blurhash
+        self.avatar_color = avatar_color
+        self.instance = Instance(**upper_to_lower(instance))
+        self.emojis = emojis
+        self.is_admin = is_admin
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        self = cls.__new__(cls)
         self.id = data.get('id')
         self.name = data.get('name', data.get('username', None))
         self.username = data.get('username')
@@ -165,8 +246,9 @@ class User(object):
         self.avatar_url = data.get('avatarUrl')
         self.avatar_blurhash = data.get('avatarBlurhash')
         self.avatar_color = data.get('avatarColor')
-        self.instance = Instance(data)
+        self.instance = Instance(**upper_to_lower(data))
         self.emojis = data.get('emojis')
+        self.is_admin = data.get('is_admin')
 
 
 class Instance(object):
@@ -179,7 +261,24 @@ class Instance(object):
         'theme_color'
     )
 
-    def __init__(self, data):
+    def __init__(self,
+                 home: str = None,
+                 name: str = None,
+                 software_name: str = None,
+                 icon_url: str = None,
+                 favicon_url=None,
+                 theme_color=None
+                 ):
+        self.home = home
+        self.name = name
+        self.software_name = software_name
+        self.icon_url = icon_url
+        self.favicon_url = favicon_url
+        self.theme_color = theme_color
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        self = cls.__new__(cls)
         self.home = data.get('instance', {}).get('home')
         self.name = data.get('instance', {}).get('name')
         self.software_name = data.get('instance', {}).get('softwareName')
