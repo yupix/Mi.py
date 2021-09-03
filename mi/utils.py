d16c068e6617854af9c80e93e8f5157a87aab4de
yupix <yupi0982@outlook.jp>
2021-09-03 10:33:28 +0900

    feat: utils.py に upper_to_lower 関数を追加

--- mi/utils.py
+++ mi/utils.py
@@ -1,6 +1,23 @@
 """
 Mi.pyを使用する上でちょっとした際に便利なツール一覧
 """
+import re
+
+
+def upper_to_lower(data: dict, field: dict = None):
+    if data is None:
+        return {}
+    replace_list: dict = {'user': 'author', 'id': 'id_'}
+    p = re.compile('[A-Z]')
+    if field is None:
+        field = {}
+    for attr in data:
+        default_key = ("_" + p.search(attr)[0].lower()).join(p.split(attr)) if p.search(attr) is not None else attr
+        if replace_list.get(attr):
+            default_key = default_key.replace(attr, replace_list.get(attr))
+
+        field[default_key] = data[attr]
+    return field
 
 
 def bool_to_string(boolean: bool) -> str:
