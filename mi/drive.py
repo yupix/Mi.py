ada93cbfc2f51aa51fa3dbfcdafe0b66aac1b437
yupix <yupi0982@outlook.jp>
2021-09-13 22:32:35 +0900

    feat: Driveをモデルに変更、DriveActionを追加し継承するように

--- mi/drive.py
+++ mi/drive.py
@@ -1,8 +1,37 @@
-import json
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
-import requests
 from pydantic import BaseModel
+
+from mi import config
+from mi.utils import api
+
+
+class DriveAction(object):
+    def upload(self, path: str, name: str = None, force: bool = False, is_sensitive: bool = False) -> 'Drive':
+        """
+
+        Parameters
+        ----------
+        is_sensitive : bool
+            この画像がセンシティブな物の場合Trueにする
+        force : bool
+            Trueの場合同じ名前のファイルがあった場合でも強制的に保存する
+        path : str
+            そのファイルまでのパスとそのファイル.拡張子(/home/test/test.png)
+        name: str
+            ファイル名(拡張子があるなら含めて)
+
+        Returns
+        -------
+        Drive: Drive
+            upload後のレスポンスをDrive型に変更して返します
+        """
+        with open(path, 'rb') as f:
+            file = f.read()
+        args = {'i': f'{config.i.token}', 'isSensitive': is_sensitive, 'force': force, 'name': f'{name}'}
+        file = {'file': file}
+        res = api(config.i.origin_uri, '/api/drive/files/create', data=args, files=file).json()
+        return Drive(**res)
 
 
 class Properties(BaseModel):
@@ -40,57 +69,14 @@ class File(BaseModel):
     user: Dict[str, Any]
 
 
-class Drive(object):
-    def __init__(self, data=None, token: str = None, origin_uri: str = None):
-        # TODO 2021-08-29 こっちでupload等の引数を受け取る様にする
-        if data is None:
-            data = {}
-        else:
-            data = json.loads(data)
-            self.id = data.get('id')
-            self.created_at = data.get('createdAt')
-            self.name = data.get('name')
-            self.type = data.get('type')
-            self.md5 = data.get('md5')
-            self.size = data.get('size')
-            self.is_sensitive = data.get('isSensitive')
-            self.blurhash = data.get('blurhash')
-            self.properties = Properties(data.get('properties', {}))
-            self.url = data.get('url')
-            self.thumbnail_url = data.get('thumbnailUrl')
-        self.token = token
-        self.origin_uri = origin_uri
-        self.path: str = ''
-        self.name: str = ''
-
-    def upload(self, path: str, name: str = None) -> 'Drive':
-        """
-
-        Parameters
-        ----------
-        path : str
-            そのファイルまでのパスとそのファイル.拡張子(/home/test/test.png)
-        name : str
-            アップロードするファイルの名前
-        Returns
-        -------
-        Drive: Drive
-            upload後のレスポンスをDrive型に変更して返します
-        """
-        self.path = path
-        self.name = name
-        with open(path, 'rb') as f:
-            file = f.read()
-        args = {'i': f'{self.token}'}
-        file = {'file': file}
-
-        res = requests.post(self.origin_uri + '/api/drive/files/create', data=args, files=file)
-        return Drive(res.text)
-
-
-class Properties(object):
-    def __init__(self, data=None):
-        if data is None:
-            data = {}
-        self.width = data.get('width')
-        self.height = data.get('height')
+class Drive(BaseModel, DriveAction):
+    id: Optional[str] = None
+    created_at: Optional[str] = None
+    name: Optional[str] = None
+    type: Optional[str] = None
+    md5: Optional[str] = None
+    size: Optional[int] = None
+    url: Optional[str] = None
+    folder_id: Optional[str] = None
+    is_sensitive: Optional[bool] = False
+    blurhash: Optional[str] = None
