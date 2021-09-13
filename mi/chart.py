2e2d1b8f043a7ce31dd53ff377db0e956cc3cd22
yupix <yupi0982@outlook.jp>
2021-09-13 16:09:47 +0900

    fix: get_active_usersメソッドの返り値が全てNone

--- mi/chart.py
+++ mi/chart.py
@@ -1,3 +1,4 @@
+import json
 from typing import List, Optional
 
 from pydantic import BaseModel
@@ -9,16 +10,16 @@ from mi.utils import api, json_dump
 class ChartAction:
     def get_active_users(self):
         data = json_dump({'span': self.span, 'limit': self.limit, 'offset': self.offset})
-        res = api(config.i.origin_uri, '/api/charts/active-users', data)
-        return Chart(**res.json())
+        res = api(config.i.origin_uri, '/api/charts/active-users', data).json()
+        return Chart(**res)
 
 
 class Local(BaseModel):
-    users: Optional[List[str]] = None
+    users: Optional[List[int]] = None
 
 
 class Remote(BaseModel):
-    users: Optional[List[str]] = None
+    users: Optional[List[int]] = None
 
 
 class Chart(BaseModel, ChartAction):
