f993c24d9dcb62409213573cc76e66836a8cb186
yupix <yupi0982@outlook.jp>
2021-09-17 18:23:24 +0900

    fix: 循環インポート

--- mi/__init__.py
+++ mi/__init__.py
@@ -8,16 +8,16 @@ __version__ = '0.1.5'
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
-from .bot import Bot, BotBase
+from .instance import Instance
+from .emoji import Emoji
+from .user import UserProfile
 from .chart import Chart, Local, Remote
 from .drive import Drive, Properties
-from .emoji import Emoji
 from .ext import task
 from .ext.task import Loop, loop
-from .instance import Instance
 from .note import Follow, Header, Note, Reaction
 from .router import Router
-from .user import UserProfile
+from .bot import Bot, BotBase
 
 __all__ = [
     'Chart',
