1214e0335e646eb32feb18c6e4c12b503d9650e4
yupix <yupi0982@outlook.jp>
2021-09-28 23:26:26 +0900

    feat: cogのload処理等を追加

--- mi/ext/commands/bot.py
+++ mi/ext/commands/bot.py
@@ -2,24 +2,26 @@
 
 import asyncio
 import importlib
-from mi.exception import CogNameDuplicate
+from mi.exception import CogNameDuplicate, ExtensionAlreadyLoaded, ExtensionFailed, ExtensionNotFound, NoEntryPointError
 from mi.user import UserAction
 import re
 import sys
 import traceback
-from typing import Any, Callable, Coroutine, Dict
+from typing import Any, Callable, Coroutine, Dict, Optional
 
 from mi import UserProfile, config
 from mi.http import WebSocket
 
 __all__ = ['BotBase', 'Bot']
 
-class BotBase(WebSocket):
+
+class BotBase:
     def __init__(self):
         self.extra_events = {}
         self.special_events = {}
         self.token = None
         self.origin_uri = None
+        self.__extensions: Dict[str, Any] = {}
         self.i: UserProfile = None
         self.__cogs: Dict[str] = {}
         super().__init__(self)
@@ -97,6 +99,54 @@ class BotBase(WebSocket):
 
         return cog
 
+    def _load_from_module_spec(self, spec: importlib.machinery.ModuleSpec, key: str) -> None:
+        lib = importlib.util.module_from_spec(spec)
+        sys.modules[key] = lib
+        try:
+            spec.loader.exec_module(lib)
+        except Exception as e:
+            del sys.modules[key]
+            raise ExtensionFailed(key, e) from e
+
+        try:
+            setup = getattr(lib, 'setup')
+        except AttributeError:
+            del sys.modules[key]
+            raise NoEntryPointError(key)
+
+        try:
+            setup(self)
+        except Exception as e:
+            del sys.modules[key]
+            # self._remove_module_references(lib.__name__)
+            # self._call_module_finalizers(lib, key)
+            raise ExtensionFailed(key, e) from e
+        else:
+            self.__extensions[key] = lib
+
+    def _resolve_name(self, name: str, package: Optional[str]) -> str:
+        try:
+            return importlib.util.resolve_name(name, package)
+        except ImportError:
+            raise ExtensionNotFound(name)
+
+    def load_extension(self, name: str, *, package: Optional[str] = None) -> None:
+        """拡張をロードする
+
+        Parameters
+        ----------
+        name : str
+            [description]
+        package : Optional[str], optional
+            [description], by default None
+        """
+        name = self._resolve_name(name, package)
+        if name in self.__extensions:
+            raise ExtensionAlreadyLoaded
+
+        spec = importlib.util.find_spec(name)
+        self._load_from_module_spec(spec, name)
+
     async def schedule_event(self, coro: Callable[..., Coroutine[Any, Any, Any]], event_name: str, *args: Any,
                              **kwargs: Any) -> asyncio.Task:
         return asyncio.create_task(self._run_event(coro, event_name, *args, **kwargs), name=f'MI.py: {event_name}')
@@ -168,8 +218,7 @@ class BotBase(WebSocket):
         auth_i = {'token': self.token, 'origin_uri': self.origin_uri}
         config.init(**auth_i)
         self.i = UserAction().get_i()
-        asyncio.get_event_loop().run_until_complete(
-            self._run(f'{uri}?i={token}'))
+        asyncio.get_event_loop().run_until_complete(WebSocket(self).run(f'{uri}?i={token}'))
 
 
 class Bot(BotBase):
