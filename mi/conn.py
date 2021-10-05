b3838cc2ee12873fec7b52f673a04f3a83ee8753
yupix <yupi0982@outlook.jp>
2021-10-05 11:52:18 +0900

    feat: get_followers関数を追加

--- mi/conn.py
+++ mi/conn.py
@@ -26,3 +26,57 @@ def get_user(user_id: str = None, username: str = None, host: str = None) -> dic
 
     data = {'userId': user_id, 'username': username, 'host': host}
     return api(config.i.origin_uri, '/api/users/show', json_data=data).json()
+
+
+def get_followers(user_id: str = None,
+                  username: str = None,
+                  host: str = None,
+                  since_id: str = None,
+                  until_id: str = None,
+                  limit: int = 10,
+                  get_all: bool = False) -> typing.Iterator[dict]:
+    """与えられたユーザーのフォロワーを取得します
+
+    Parameters
+    ----------
+    user_id : str, default=None
+        ユーザーのid
+    username : str, default=None
+        ユーザー名
+    host : str, default=None
+        ユーザーがいるインスタンスのhost名
+    since_id : str, default=None
+    until_id : str, default=None
+        前回の最後の値を与える(既に実行し取得しきれない場合に使用)
+    limit : int, default=10
+        取得する情報の最大数 max: 100
+    get_all : bool, default=False
+        全てのフォロワーを取得する
+
+    Returns
+    -------
+    dict
+        フォロワーの情報
+
+    Raises
+    ------
+    InvalidParameters
+        limit引数が不正な場合
+    """
+    if not check_multi_arg(user_id, username):
+        raise NotExistRequiredParameters('user_id, usernameどちらかは必須です')
+
+    if limit > 100:
+        raise InvalidParameters('limit は100以上を受け付けません')
+
+    data = remove_dict_empty(
+        {'userId': user_id, 'username': username, 'host': host, 'sinceId': since_id, 'untilId': until_id, 'limit': limit})
+    if get_all:
+        loop = True
+        while loop:
+            get_data = api(config.i.origin_uri, '/api/users/followers', json_data=data).json()
+            if len(get_data) > 0:
+                data['untilId'] = get_data[-1]['id']
+            else:
+                break
+            yield get_data
