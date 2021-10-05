237c079a26b6c0b1e65a914cd31ace356088ca1f
yupix <yupi0982@outlook.jp>
2021-10-05 11:38:50 +0900

    feat: check_multi_arg関数を追加

--- mi/utils.py
+++ mi/utils.py
@@ -11,6 +11,22 @@ import requests
 T = TypeVar('T')
 
 
+def check_multi_arg(*args) -> bool:
+    """複数の値を受け取り値が存在するかをboolで返します
+
+    Parameters
+    ----------
+    args : list
+        確認したい変数のリスト
+
+    Returns
+    -------
+    bool
+        存在する場合はTrue, 存在しない場合はFalse
+    """
+    return bool([i for i in args if i])
+
+
 async def async_all(gen, *, check=isawaitable):
     for elem in gen:
         if check(elem):
