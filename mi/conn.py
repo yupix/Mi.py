361deccefe58ebd2fec863228894c23f7a4765bb
yupix <yupi0982@outlook.jp>
2021-10-05 11:51:49 +0900

    chore: get_userでuserIdもしくはuseranemeが無い際エラーを出す

--- mi/conn.py
+++ mi/conn.py
@@ -1,7 +1,28 @@
+import typing
+
 from mi import config
-from mi.utils import api
+from mi.exception import InvalidParameters, NotExistRequiredParameters
+from mi.utils import api, check_multi_arg, remove_dict_empty
 
 
-def get_user(user_id: str, username: str, host: str = None) -> dict:
+def get_user(user_id: str = None, username: str = None, host: str = None) -> dict:
+    """
+    Parameters
+    ----------
+    user_id : str
+        取得したいユーザーのユーザーID
+    username : str
+        取得したいユーザーのユーザー名
+    host : str, default=None
+        取得したいユーザーがいるインスタンスのhost
+
+    Returns
+    -------
+    dict:
+        ユーザー情報
+    """
+    if not check_multi_arg(user_id, username):
+        raise NotExistRequiredParameters('user_id, usernameどちらかは必須です')
+
     data = {'userId': user_id, 'username': username, 'host': host}
     return api(config.i.origin_uri, '/api/users/show', json_data=data).json()
