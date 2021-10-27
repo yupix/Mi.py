1389762fc97bfeaa1c927efe012731f7bbabd9c7
yupix <yupi0982@outlook.jp>
2021-10-27 14:26:05 +0900

    chore: instanceにhostが不足してる

--- mi/types/instance.py
+++ mi/types/instance.py
@@ -2,6 +2,7 @@ from typing import Optional, TypedDict
 
 
 class Instance(TypedDict):
+    host: Optional[str]
     name: Optional[str]
     software_name: Optional[str]
     software_version: Optional[str]
