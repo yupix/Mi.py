fac7fad2e52141cda127920dddfe0a9e78e01282
yupix <yupi0982@outlook.jp>
2021-11-02 20:54:51 +0900

    fix: software_nameの部分でsoftware_versionを取得してた

--- mi/instance.py
+++ mi/instance.py
@@ -5,7 +5,7 @@ class Instance:
     def __init__(self, data: InstancePayload):
         self.host: str = data['host']
         self.name: str = data['name']
-        self.software_name: str = data['software_version']
+        self.software_name: str = data['software_name']
         self.software_version: str = data['software_version']
         self.icon_url: str = data['icon_url']
         self.favicon_url: str = data['favicon_url']
