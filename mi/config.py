990cfb46d218b3bdb999a431b27cd9228e8de799
yupix <yupi0982@outlook.jp>
2021-11-02 21:19:44 +0900

    chore: 型を正しいものに変更

--- mi/config.py
+++ mi/config.py
@@ -13,8 +13,8 @@ class Config:
     Botを動作させる上でのConfig
     """
 
-    def __init__(self, token: str, origin_uri: str, profile: Any = None, instance: Any = None):
+    def __init__(self, token: str, origin_uri: str, profile: Any = None, instance: dict = None):
         self.token: str = token
         self.origin_uri: str = origin_uri
         self.profile: Any = profile
-        self.instance: Any = instance
+        self.instance: dict = instance
