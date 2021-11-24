6d50193c117e1d7d82e7b23e2a1c393f18258b4a
yupix <yupi0982@outlook.jp>
2021-11-24 18:24:45 +0900

    chore: type:emojiでcategoryやidを追加

--- mi/types/emoji.py
+++ mi/types/emoji.py
@@ -2,7 +2,9 @@ from typing import List, Optional, TypedDict
 
 
 class Emoji(TypedDict):
-    name: Optional[str]
-    url: Optional[str]
-    host: Optional[str]
+    id: Optional[str]
     aliases: Optional[List[str]]
+    name: Optional[str]
+    category: Optional[str]
+    host: Optional[str]
+    url: Optional[str]
