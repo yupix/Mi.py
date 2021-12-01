8e06beb06308b9b3c88f0ecbe33cb375fab2fbf0
yupix <yupi0982@outlook.jp>
2021-12-01 23:43:33 +0900

    refactor: cogシステムを書き直し&TypeHintの追加 close #15

--- mi/ext/commands/cog.py
+++ mi/ext/commands/cog.py
@@ -1,103 +1,39 @@
 import inspect
-from typing import Any, ClassVar, Dict, List, Tuple
+from typing import Any, Callable, ClassVar, Coroutine, Dict, List, Optional, Tuple, Union
+from mi.abc.ext.bot import AbstractBotBase
 
-__all__ = ["Cog"]
-
-from ._types import _BaseCommand
+from mi.ext.commands.core import Command
+from mi.ext.commands._types import _BaseCommand
 
 
 class CogMeta(type):
-    """A metaclass for defining a cog.
-
-    Note that you should probably not use this directly. It is exposed
-    purely for documentation purposes along with making custom metaclasses to intermix
-    with other metaclasses such as the :class:`abc.ABCMeta` metaclass.
-
-    For example, to create an abstract cog mixin class, the following would be done.
-
-    .. code-block:: python3
-
-        import abc
-
-        class CogABCMeta(commands.CogMeta, abc.ABCMeta):
-            pass
-
-        class SomeMixin(metaclass=abc.ABCMeta):
-            pass
-
-        class SomeCogMixin(SomeMixin, commands.Cog, metaclass=CogABCMeta):
-            pass
-
-    .. note::
-
-        When passing an attribute of a metaclass that is documented below, note
-        that you must pass it as a keyword-only argument to the class creation
-        like the following example:
-
-        .. code-block:: python3
-
-            class MyCog(commands.Cog, name='My Cog'):
-                pass
-
-    Attributes
-    -----------
-    name: :class:`str`
-        The cog name. By default, it is the name of the class with no modification.
-    description: :class:`str`
-        The cog description. By default, it is the cleaned docstring of the class.
-
-        .. versionadded:: 1.6
-
-    command_attrs: :class:`dict`
-        A list of attributes to apply to every command inside this cog. The dictionary
-        is passed into the :class:`Command` options at ``__init__``.
-        If you specify attributes inside the command attribute in the class, it will
-        override the one specified inside this attribute. For example:
-
-        .. code-block:: python3
-
-            class MyCog(commands.Cog, command_attrs=dict(hidden=True)):
-                @commands.command()
-                async def foo(self, ctx):
-                    pass # hidden -> True
-
-                @commands.command(hidden=False)
-                async def bar(self, ctx):
-                    pass # hidden -> False
-    """
-
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls, *args: Tuple[Any], **kwargs: Dict[str, Any]):
         name, bases, attrs = args
-        attrs["__cog_name__"] = kwargs.pop("name", name)
-        attrs["__cog_settings__"] = kwargs.pop("command_attrs", {})
-
-        description = kwargs.pop("description", None)
-        if description is None:
-            description = inspect.cleandoc(attrs.get("__doc__", ""))
-        attrs["__cog_description__"] = description
-
+        attrs['__cog_name__'] = kwargs.pop("name", name)
+        attrs['__cog_settings__'] = kwargs.pop("command_attrs", {})
         commands = {}
         listeners = {}
         no_bot_cog = "Commands or listeners must not start with cog_ or bot_ (in method {0.__name__}.{1})"
-
         new_cls = super().__new__(cls, name, bases, attrs, **kwargs)
-        for base in reversed(new_cls.__mro__):
+
+        for base in reversed(new_cls.__mro__):  # 多重継承を確認
             for elem, value in base.__dict__.items():
                 if elem in commands:
-                    del commands[elem]  # commandsから消す
+                    del commands[elem]  # commandsから削除
                 if elem in listeners:
-                    del listeners[elem]
+                    del listeners[elem]  # listenersから削除
 
                 is_static_method = isinstance(value, staticmethod)
-                if is_static_method:
-                    value = value.__func__
-                # value が重要
+
+                if is_static_method:  # staticmethodか確認
+                    value = value.__func__  # 関数をvalueに !valueが重要
+
                 if isinstance(value, _BaseCommand):
                     if is_static_method:
                         raise TypeError(
                             f"Command in method {base}.{elem!r} must not be staticmethod."
                         )
-                    if elem.startswith(("cog_", "bot_")):
+                    if elem.startswith(("cog", "bot_")):
                         raise TypeError(no_bot_cog.format(base, elem))
                     commands[elem] = value
                 elif inspect.iscoroutinefunction(value):
@@ -106,132 +42,80 @@ class CogMeta(type):
                     except AttributeError:
                         continue
                     else:
-                        if elem.startswith(("cog_", "bot_")):
+                        if elem.startswith(("cog", "bot_")):
                             raise TypeError(no_bot_cog.format(base, elem))
                         listeners[elem] = value
 
-        new_cls.__cog_commands__ = list(
-            commands.values()
-        )  # this will be copied in Cog.__new__
+        new_cls.__cog_commands__ = list(commands.values())
 
-        listeners_as_list = []
+        listeners_as_list: List[tuple[str, Any]] = []
+
         for listener in listeners.values():
             for listener_name in listener.__cog_listener_names__:
-                # I use __name__ instead of just storing the value so I can inject
-                # the self attribute when the time comes to add them to the bot
-                listeners_as_list.append((listener_name, listener.__name__))
+                listeners_as_list.append((listener_name, listener))
 
         new_cls.__cog_listeners__ = listeners_as_list
         return new_cls
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args)
-
-    @classmethod
-    def qualified_name(cls):
-        return cls.__cog_name__
+    def __init__(self, *args: Tuple[Any], **kwargs: Dict[str, Any]):
+        super().__init__(*args, **kwargs)
 
 
 class Cog(metaclass=CogMeta):
     __cog_name__ = ClassVar[str]
-    __cog_settings__ = ClassVar[Dict[str, Any]]
-    __cog_commands__: ClassVar[List]
+    __cog_settings__: Dict[str, Any] = {}
+    __cog_commands__: List[Command] = []
     __cog_listeners__: ClassVar[List[Tuple[str, str]]]
 
-    def __new__(cls, *args, **kwargs):
-        # For issue 426, we need to store a copy of the command objects
-        # since we modify them to inject `self` to them.
-        # To do this, we need to interfere with the Cog creation process.
+    def __new__(cls, *args: tuple[Any], **kwargs: Dict[str, Any]):
         self = super().__new__(cls)
         cmd_attrs = cls.__cog_settings__
-
-        # Either update the command with the cog provided defaults or copy it.
-        self.__cog_commands__ = tuple(
-            c._update_copy(cmd_attrs) for c in cls.__cog_commands__
-        )
+        self.__cog_commands__ = tuple(c._update_copy(cmd_attrs) for c in cls.__cog_commands__)
 
         lookup = {cmd.qualified_name: cmd for cmd in self.__cog_commands__}
 
-        # Update the Command instances dynamically as well
         for command in self.__cog_commands__:
             setattr(self, command.callback.__name__, command)
             parent = command.parent
-            if parent is not None:
-                # Get the latest parent reference
+            if parent:
                 parent = lookup[parent.qualified_name]
 
-                # Update our parent's reference to our self
                 parent.remove_command(command.name)
                 parent.add_command(command)
-
         return self
 
     @classmethod
-    def listener(cls, name=None):
-        """A decorator that marks a function as a listener.
-
-        This is the cog equivalent of :meth:`.Bot.listen`.
-
-        Parameters
-        ------------
-        name: :class:`str`
-            The name of the event being listened to. If not provided, it
-            defaults to the function's name.
-
-        Raises
-        --------
-        TypeError
-            The function is not a coroutine function or a string was not passed as
-            the name.
-        """
-        if name is not None and not isinstance(name, str):
-            raise TypeError(
-                f"Cog.listener expected str but received {name.__class__.__name__!r} instead."
-            )
-
-        def decorator(func):
+    def listener(cls, name: Optional[str]=None):
+        if name is not None:
+            raise TypeError(f'Cog.listener expected str but received {name.__clsss__.__name__!r} instead.')
+        
+        def decorator(func: Callable[..., Coroutine[Any, Any, Any]]):
             actual = func
             if isinstance(actual, staticmethod):
                 actual = actual.__func__
             if not inspect.iscoroutinefunction(actual):
-                raise TypeError("Listener function must be a coroutine function.")
+                raise TypeError('Listener function must be a coroutine function.')
             actual.__cog_listener__ = True
             to_assign = name or actual.__name__
             try:
                 actual.__cog_listener_names__.append(to_assign)
             except AttributeError:
                 actual.__cog_listener_names__ = [to_assign]
-            # we have to return `func` instead of `actual` because
-            # we need the type to be `staticmethod` for the metaclass
-            # to pick it up but the metaclass unfurls the function and
-            # thus the assignments need to be on the actual function
             return func
-
         return decorator
 
-    def _inject(self, bot):
+    def _inject(self, bot: AbstractBotBase):
         cls = self.__class__
-
-        # realistically, the only thing that can cause loading errors
-        # is essentially just the command loading, which raises if there are
-        # duplicates. When this condition is met, we want to undo all what
-        # we've added so far for some form of atomic loading.
+        print(self.__cog_commands__)
         for index, command in enumerate(self.__cog_commands__):
-            command.cog = self
             if command.parent is None:
                 try:
                     bot.add_command(command)
                 except Exception as e:
-                    # undo our additions
                     for to_undo in self.__cog_commands__[:index]:
                         if to_undo.parent is None:
                             bot.remove_command(to_undo.name)
-                    raise e
-
-        # while Bot.add_listener can raise if it's not a coroutine,
-        # this precondition is already met by the listener decorator
-        # already, thus this should never raise.
-        # Outside of, memory errors and the like...
+                        raise e
         for name, method_name in self.__cog_listeners__:
             bot.add_listener(getattr(self, method_name), name)
 
--- mi/ext/commands/core.py
+++ mi/ext/commands/core.py
@@ -1,37 +1,18 @@
-__all__ = ["Command", "command", "GroupMixin", "group"]
-
 import asyncio
 import functools
-from typing import Any, Dict
+from typing import Any, Callable, Coroutine, Dict, Optional, Tuple, TypeVar, Union
+from typing_extensions import Concatenate
 
-from ...exception import CommandError, CommandInvokeError, CommandRegistrationError
 from ._types import _BaseCommand
-
-
-class _CaseInsensitiveDict(dict):
-    def __contains__(self, k):
-        return super().__contains__(k.casefold())
-
-    def __delitem__(self, k):
-        return super().__delitem__(k.casefold())
-
-    def __getitem__(self, k):
-        return super().__getitem__(k.casefold())
-
-    def get(self, k, default=None):
-        return super().get(k.casefold(), default)
-
-    def pop(self, k, default=None):
-        return super().pop(k.casefold(), default)
-
-    def __setitem__(self, k, v):
-        super().__setitem__(k.casefold(), v)
+from mi.exception import CommandError, CommandInvokeError, CommandRegistrationError
+from ...abc.ext.core import AbstractCommand, AbstractGroup
 
 
 class GroupMixin:
     def __init__(self, *args: tuple[Any], **kwargs: Dict[Any, Any]):
-        case_insensitive = kwargs.get("case_insensitive", False)
-        self.all_commands = _CaseInsensitiveDict() if case_insensitive else {}
+        # TODO: case_insensitiveの実態調査してから実装するか考慮する
+        case_insensitive = kwargs.get("case_insensitive", False) 
+        self.all_commands: Dict[str, Command] = {}
         self.case_insensitive = case_insensitive
         super().__init__(*args, **kwargs)
 
@@ -46,34 +27,15 @@ class GroupMixin:
                 command.recursively_remove_all_commands()
             self.remove_command(command.name)
 
-    def add_command(self, command):
-        """Adds a :class:`.Command` into the internal list of commands.
-
-        This is usually not called, instead the :meth:`~.GroupMixin.command` or
-        :meth:`~.GroupMixin.group` shortcut decorators are used instead.
-
-        .. versionchanged:: 1.4
-             Raise :exc:`.CommandRegistrationError` instead of generic :exc:`.ClientException`
-
-        Parameters
-        -----------
-        command: :class:`Command`
-            The command to add.
-
-        Raises
-        -------
-        :exc:`.CommandRegistrationError`
-            If the command or its alias is already registered by different command.
-        TypeError
-            If the command passed is not a subclass of :class:`.Command`.
-        """
+    def add_command(self, command:AbstractCommand):
+        print('呼ばれた')
         if not isinstance(command, Command):
-            raise TypeError("The command passed must be a subclass of Command")
+            raise TypeError(f'{command} passed must be a subclass of Command')
 
         if isinstance(self, Command):
             command.parent = self
 
-        if command.name in self.all_commands:
+        if command.name is self.all_commands:
             raise CommandRegistrationError(command.name)
 
         self.all_commands[command.name] = command
@@ -83,10 +45,32 @@ class GroupMixin:
                 raise CommandRegistrationError(alias, alias_conflict=True)
             self.all_commands[alias] = command
 
+    def remove_command(self, name:str):
+        command = self.all_commands.pop(name, None)
+        if command is None:
+            return None
+        
+        if name in command.aliases:
+            return command
+        
+        for alias in command.aliases:
+            cmd = self.all_commands.pop(alias, None)
+            if cmd is not None and cmd != command:
+                self.all_commands[alias] = cmd
+        return command
 
-def hooked_wrapped_callback(command, ctx, coro):
+    def command(self, name: Optional[str] = None, cls=None, *args: Tuple[Any], **kwargs: Dict[Any, Any]):
+        def decorator(func: Callable[..., Any]):  # TODO:ここやる
+            kwargs.setdefault('parent', self)
+            result = command(name=name, cls=cls, *args, **kwargs)(func)
+            return result
+
+        return decorator
+
+
+def hooked_wrapped_callback(command: 'Command', ctx, coro):
     @functools.wraps(coro)
-    async def wrapped(*args, **kwargs):
+    async def wrapped(*args: Tuple[Any], **kwargs: Dict[Any,Any]):
         try:
             ret = await coro(ctx, *args, **kwargs)
         except CommandError:
@@ -105,80 +89,58 @@ def hooked_wrapped_callback(command, ctx, coro):
     return wrapped
 
 
-class Command(_BaseCommand):  # TODO: 作る
-    def __init__(self, func, **kwargs):
+class Command(_BaseCommand, AbstractCommand):
+    def __init__(self, func: Callable[..., Coroutine[Any, Any, Any]], **kwargs: Any):
         if not asyncio.iscoroutinefunction(func):
-            raise TypeError("関数は非同期である必要があります")
-        self.name = name = kwargs.get("name") or func.__name__
+            raise TypeError('func must be a coroutine')
+        self.name = name = kwargs.get('name') or func.__name__
         if not isinstance(name, str):
-            raise TypeError("コマンドのネームスペースはstringである必要があります")
+            raise TypeError('name must be a string')
 
         self.callback = func
-        self.enabled = kwargs.get("enabled", True)
+        self.enabled = kwargs.get('enabled', True)
         self.__original_kwargs__ = kwargs.copy()
-        self.aliases = kwargs.get("aliases", [])
+        self.aliases = kwargs.get('aliases', [])
 
         try:
             checks = func.__commands_checks__
             checks.reverse()
         except AttributeError:
-            checks = kwargs.get("checks", [])
+            checks = kwargs.get('checks', [])
         finally:
             self.checks = checks
         self.cog = None
-        parent = kwargs.get("parent")
-        self.parent = parent if isinstance(parent, _BaseCommand) else None
+        parent = kwargs.get('parent')
+        self.parent = parent if isinstance(parent, _BaseCommand) else None  # TODO: parentの型調べる
 
     @property
-    def full_parent_name(self):
-        """:class:`str`: Retrieves the fully qualified parent command name.
-
-        This the base command name required to execute it. For example,
-        in ``?one two three`` the parent name would be ``one two``.
-        """
+    def full_parent_name(self) -> str:
         entries = []
         command = self
         while command.parent is not None:
             command = command.parent
             entries.append(command.name)
-
-        return " ".join(reversed(entries))
+        return ' '.join(reversed(entries))
 
     @property
-    def qualified_name(self):
-        """:class:`str`: Retrieves the fully qualified command name.
-
-        This is the full parent name with the command name as well.
-        For example, in ``?one two three`` the qualified name would be
-        ``one two three``.
-        """
+    def qualified_name(self) -> str:
         parent = self.full_parent_name
         if parent:
-            return parent + " " + self.name
+            return parent + ' ' + self.name
         return self.name
 
     async def call_after_hooks(self, ctx):
-        # cog = self.cog
-        # call the cog local hook if applicable:
-        # if cog is not None:
-        #    hook = Cog._get_overridden_method(cog.cog_after_invoke)
-        #    if hook is not None:
-        #        await hook(ctx)
-
         hook = ctx.bot._after_invoke
         if hook is not None:
             await hook(ctx)
 
-    async def invoke(self, ctx, *args, **kwargs):
-        # terminate the invoked_subcommand chain.
-        # since we're in a regular command (and not a group) then
-        # the invoked subcommand is None.
+    async def invoke(self, ctx, *args: Tuple[Any], **kwargs: Dict[Any, Any]):
         ctx.invoked_subcommand = None
         ctx.subcommand_passed = None
         injected = hooked_wrapped_callback(self, ctx, self.callback)
         await injected(ctx, *args, **kwargs)
 
-    def _ensure_assignment_on_copy(self, other):
+    def _ensure_assignment_on_copy(self, other: 'Command') -> 'Command':
         if self.checks != other.checks:
             other.checks = self.checks.copy()
 
@@ -188,18 +150,11 @@ class Command(_BaseCommand):  # TODO: 作る
             pass
         return other
 
-    def copy(self):
-        """Creates a copy of this command.
-
-        Returns
-        --------
-        :class:`Command`
-            A new instance of this command.
-        """
+    def copy(self) -> 'Command':
         ret = self.__class__(self.callback, **self.__original_kwargs__)
         return self._ensure_assignment_on_copy(ret)
 
-    def _update_copy(self, kwargs):
+    def _update_copy(self, kwargs: Dict[Any, Any]) -> 'Command':
         if kwargs:
             kw = kwargs.copy()
             kw.update(self.__original_kwargs__)
@@ -208,168 +163,54 @@ class Command(_BaseCommand):  # TODO: 作る
         return self.copy()
 
 
-class Group(GroupMixin, Command):
-    """A class that implements a grouping protocol for commands to be
-    executed as subcommands.
-
-    This class is a subclass of :class:`.Command` and thus all options
-    valid in :class:`.Command` are valid in here as well.
-
-    Attributes
-    -----------
-    invoke_without_command: :class:`bool`
-        Indicates if the group callback should begin parsing and
-        invocation only if no subcommand was found. Useful for
-        making it an error handling function to tell the user that
-        no subcommand was found or to have different functionality
-        in case no subcommand was found. If this is ``False``, then
-        the group callback will always be invoked first. This means
-        that the checks and the parsing dictated by its parameters
-        will be executed. Defaults to ``False``.
-    case_insensitive: :class:`bool`
-        Indicates if the group's commands should be case insensitive.
-        Defaults to ``False``.
-    """
-
-    def __init__(self, *args, **attrs):
-        self.invoke_without_command = attrs.pop(
-            "invoke_without_command", False)
+class Group(GroupMixin, Command, AbstractGroup):
+    def __init__(self, *args: tuple[Any], **attrs: Any):
+        self.invoke_without_command: bool = attrs.pop('invoke_without_command', False)
         super().__init__(*args, **attrs)
 
-    def copy(self):
-        """Creates a copy of this :class:`Group`.
 
-        Returns
-        --------
-        :class:`Group`
-            A new instance of this group.
-        """
-        ret = super().copy()
-        for cmd in self.commands:
-            ret.add_command(cmd.copy())
-        return ret
+def group(name: Optional[str] = None, **attrs: Any):
+    """group decorator
 
-    async def invoke(self, ctx):
-        ctx.invoked_subcommand = None
-        ctx.subcommand_passed = None
-        early_invoke = not self.invoke_without_command
-        if early_invoke:
-            await self.prepare(ctx)
-
-        view = ctx.view
-        previous = view.index
-        view.skip_ws()
-        trigger = view.get_word()
-
-        if trigger:
-            ctx.subcommand_passed = trigger
-            ctx.invoked_subcommand = self.all_commands.get(trigger, None)
-
-        if early_invoke:
-            injected = hooked_wrapped_callback(self, ctx, self.callback)
-            await injected(*ctx.args, **ctx.kwargs)
-
-        ctx.invoked_parents.append(ctx.invoked_with)
-
-        if trigger and ctx.invoked_subcommand:
-            ctx.invoked_with = trigger
-            await ctx.invoked_subcommand.invoke(ctx)
-        elif not early_invoke:
-            # undo the trigger parsing
-            view.index = previous
-            view.previous = previous
-            await super().invoke(ctx)
-
-    async def reinvoke(self, ctx, *, call_hooks=False):
-        ctx.invoked_subcommand = None
-        early_invoke = not self.invoke_without_command
-        if early_invoke:
-            ctx.command = self
-            await self._parse_arguments(ctx)
-
-            if call_hooks:
-                await self.call_before_hooks(ctx)
-
-        view = ctx.view
-        previous = view.index
-        view.skip_ws()
-        trigger = view.get_word()
-
-        if trigger:
-            ctx.subcommand_passed = trigger
-            ctx.invoked_subcommand = self.all_commands.get(trigger, None)
-
-        if early_invoke:
-            try:
-                await self.callback(*ctx.args, **ctx.kwargs)
-            except:
-                ctx.command_failed = True
-                raise
-            finally:
-                if call_hooks:
-                    await self.call_after_hooks(ctx)
-
-        ctx.invoked_parents.append(ctx.invoked_with)
-
-        if trigger and ctx.invoked_subcommand:
-            ctx.invoked_with = trigger
-            await ctx.invoked_subcommand.reinvoke(ctx, call_hooks=call_hooks)
-        elif not early_invoke:
-            # undo the trigger parsing
-            view.index = previous
-            view.previous = previous
-            await super().reinvoke(ctx, call_hooks=call_hooks)
-
-
-def group(name=None, **attrs):
-    """A decorator that transforms a function into a :class:`.Group`.
-
-    This is similar to the :func:`.command` decorator but the ``cls``
-    parameter is set to :class:`Group` by default.
-
-    .. versionchanged:: 1.1
-        The ``cls`` parameter can now be passed.
-    """
-    attrs.setdefault("cls", Group)
-    return command(name=name, **attrs)
-
-
-def command(name=None, cls=None, **attrs):
-    """A decorator that transforms a function into a :class:`.Command`
-    or if called with :func:`.group`, :class:`.Group`.
-
-    By default the ``help`` attribute is received automatically from the
-    docstring of the function and is cleaned up with the use of
-    ``inspect.cleandoc``. If the docstring is ``bytes``, then it is decoded
-    into :class:`str` using utf-8 encoding.
-
-    All checks added using the :func:`.check` & co. decorators are added into
-    the function. There is no way to supply your own checks through this
-    decorator.
+    サブコマンドを作成する際に使用します。
 
     Parameters
-    -----------
-    name: :class:`str`
-        The name to create the command with. By default this uses the
-        function name unchanged.
-    cls
-        The class to construct with. By default this is :class:`.Command`.
-        You usually do not change this.
-    attrs
-        Keyword arguments to pass into the construction of the class denoted
-        by ``cls``.
+    ----------
+    name : Optional[str], optional
+        コマンド名, by default None
+    """
+    attrs.setdefault("cls", Group)
+    return command(name=name, cls=Group)
+
+
+def command(name: Optional[str] = None, cls: Any = None, **attrs: Dict[str, Any]):
+    """command Decorator
+
+    コマンドの作成に用います。
+
+    Parameters
+    ----------
+    name : Optional[str], optional
+        コマンドの名前, by default None
+    cls : Any, optional
+        自前のクラスを使用する場合, by default None
+
+    Returns
+    -------
+    Command
+        コマンドクラス
 
     Raises
-    -------
+    ------
     TypeError
-        If the function is not a coroutine or is already a command.
+        Callback is already a command.
     """
     if cls is None:
         cls = Command
 
-    def decorator(func):
+    def decorator(func: Callable[..., Coroutine[Any, Any, Any]]):
         if isinstance(func, Command):
-            raise TypeError("Callback is already a command.")
+            raise TypeError('Callback is already a command.')
         return cls(func, name=name, **attrs)
 
     return decorator
