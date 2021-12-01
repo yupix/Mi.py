54a00275f2cf74c5a48e1944f9b58041b5744aeb
yupix <yupi0982@outlook.jp>
2021-12-01 16:23:29 +0900

    feat: tasksで独自のloopを受け取れるように

--- mi/ext/tasks/__init__.py
+++ mi/ext/tasks/__init__.py
@@ -5,13 +5,14 @@ from mi.exception import TaskNotRunningError
 __all__ = ["Loop", "loop"]
 
 
-
 class Loop:
-    def __init__(self, func: Callable[..., Coroutine[Any, Any, Any]], seconds: int = 60):
+    def __init__(self, func: Callable[..., Coroutine[Any, Any, Any]], seconds: int = 60,
+                 custom_loop: Optional[asyncio.AbstractEventLoop] = None):
         self.seconds: int = seconds
         self.func: Callable[..., Coroutine[Any, Any, Any]] = func
         self._task: Optional[asyncio.Task[Any]] = None
         self.stop_next_iteration = None
+        self._loop: Optional[asyncio.AbstractEventLoop] = custom_loop
 
     def start(self, *args: tuple[Any], **kwargs: Dict[Any, Any]) -> asyncio.Task[Any]:
         """
@@ -26,7 +27,7 @@ class Loop:
         -------
         _task : asyncio.Task[Any]
         """
-        _loop = asyncio.get_running_loop()
+        _loop = asyncio.get_running_loop() if self._loop is None else self._loop  # self._loop が無いなら取得
         self._task = _loop.create_task(self.task(*args, **kwargs))
         return self._task
 
@@ -53,8 +54,8 @@ class Loop:
             await asyncio.sleep(self.seconds)
 
 
-def loop(n: int):
+def loop(n: int, custom_loop: Optional[asyncio.AbstractEventLoop] = None):
     def _deco(f: Callable[..., Coroutine[Any, Any, Any]]) -> Loop:
-        return Loop(f, n)
+        return Loop(f, n, custom_loop=custom_loop)
 
     return _deco
