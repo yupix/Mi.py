5889cad470818106fc4c0bddf0b47f0cab8b735a
yupix <yupi0982@outlook.jp>
2021-12-09 15:16:07 +0900

    feat: reaction系も動くように

---
+++ mi/gateway.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+import json
+import uuid
+from typing import TYPE_CHECKING, Any, Callable, Dict, Optional
+
+import aiohttp
+
+from . import config
+if TYPE_CHECKING:
+    from .client import Client
+
+
+class MisskeyClientWebSocketResponse(aiohttp.ClientWebSocketResponse):
+    async def close(self, *, code: int = 4000, message: bytes = b'') -> bool:
+        return await super().close(code=code, message=message)
+
+
+class MisskeyWebSocket:
+    def __init__(self, socket):
+        self.socket = socket
+        self._dispatch = lambda *args: None
+        self._connection = None
+        self._misskey_parsers: Optional[Dict[str, Callable[..., Any]]] = None
+
+    @classmethod
+    async def from_client(cls, client: Client):
+        socket = await client.http.ws_connect(client.url+f'?i={config.i.token}')
+        ws = cls(socket)
+        ws._dispatch = client.dispatch
+        ws._connection = client._connection
+        ws._misskey_parsers = client._connection.parsers
+        client.dispatch('ready', socket)
+        await ws.poll_event()
+        return ws
+
+    async def received_message(self, msg, /):
+        if type(msg) is bytes:
+            msg = msg.decode()
+        # print(self._misskey_parsers)
+        self._misskey_parsers[msg['type'].upper()](msg)
+
+    async def poll_event(self):
+        msg = await self.socket.receive(timeout=60)  # TODO: いつか変数に
+        if msg.type is aiohttp.WSMsgType.TEXT:
+            await self.received_message(json.loads(msg.data))
