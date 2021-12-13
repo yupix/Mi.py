6c6178bba5703e6fb77cc4515cb4125170b304aa
yupix <yupi0982@outlook.jp>
2021-12-13 20:49:31 +0900

    feat: InstanceMetaオブジェクトを追加

--- mi/instance.py
+++ mi/instance.py
@@ -1,12 +1,56 @@
 from __future__ import annotations
 
-from typing import Iterator, Optional, TYPE_CHECKING
+from typing import Dict, Iterator, List, Optional, TYPE_CHECKING
 
-from .types.instance import Instance as InstancePayload
+from .types.instance import (Instance as InstancePayload,
+                             Meta as InstanceMetaPayload)
+from .emoji import Emoji
 
 if TYPE_CHECKING:
     from .user import User
     from . import ConnectionState
+
+
+class InstanceMeta:
+    def __init__(self, data: InstanceMetaPayload, state: ConnectionState):
+        print(data)
+        self.maintainer_name: str = data['maintainer_name']
+        self.maintainer_email: str = data['maintainer_email']
+        self.version: str = data['version']
+        self.name: str = data['name']
+        self.uri: str = data['uri']
+        self.description: str = data['description']
+        self.langs: List[str] = data['langs']
+        self.tos_url: Optional[str] = data['tos_url']
+        self.repository_url: str = data['repository_url']
+        self.feedback_url: str = data['feedback_url']
+        self.secure: bool = bool(data['secure'])
+        self.disable_registration: bool = bool(data['disable_registration'])
+        self.disable_local_timeline: bool = bool(data['disable_local_timeline'])
+        self.disable_global_timeline: bool = bool(data['disable_global_timeline'])
+        self.drive_capacity_per_local_user_mb: int = data['drive_capacity_per_local_user_mb']
+        self.drive_capacity_per_remote_user_mb: int = data['drive_capacity_per_remote_user_mb']
+        self.email_required_for_signup: bool = bool(data['email_required_for_signup'])
+        self.enable_hcaptcha: bool = bool(data['enable_hcaptcha'])
+        self.enable_recaptcha: bool = bool(data['enable_recaptcha'])
+        self.recaptcha_site_key: str = data['recaptcha_site_key']
+        self.sw_publickey: str = data['sw_publickey']
+        self.mascot_image_url: str = data['mascot_image_url']
+        self.error_image: str = data['error_image_url']
+        self.max_note_text_length: int = data['max_note_text_length']
+        self.emojis: List[Emoji] = [Emoji(i, state=state) for i in data['emojis']]
+        self.ads: list = data['ads']
+        self.enable_email: bool = bool(data['enable_email'])
+        self.enable_twitter_integration = bool(data['enable_twitter_integration'])
+        self.enable_github_integration: bool = bool(data['enable_github_integration'])
+        self.enable_discord_integration: bool = bool(data['enable_discord_integration'])
+        self.enable_service_worker: bool = bool(data['enable_service_worker'])
+        self.translator_available: bool = bool(data['translator_available'])
+        self.pinned_page: Optional[List[str]] = data.get('pinned_page')
+        self.cache_remote_files: Optional[bool] = data.get('cache_remote_files')
+        self.proxy_remote_files: Optional[bool] = data.get('proxy_remote_files')
+        self.require_setup: Optional[bool] = data.get('require_setup')
+        self.features: Optional[Dict[str, bool]] = data.get('features')
 
 
 class Instance:
--- mi/types/instance.py
+++ mi/types/instance.py
@@ -1,11 +1,78 @@
-from typing import Optional, TypedDict
+from __future__ import annotations
+from typing import List, Optional, TYPE_CHECKING, TypedDict
+
+if TYPE_CHECKING:
+    from mi.types import Emoji
 
 
-class Instance(TypedDict):
-    host: Optional[str]
-    name: Optional[str]
-    software_name: Optional[str]
-    software_version: Optional[str]
-    icon_url: Optional[str]
-    favicon_url: Optional[str]
-    theme_color: Optional[str]
+class Features:
+    registration: bool
+    local_time_line: bool
+    global_time_line: bool
+    email_required_for_signup: bool
+    elasticsearch: bool
+    hcaptcha: bool
+    recaptcha: bool
+    object_storage: bool
+    twitter: bool
+    github: bool
+    github: bool
+    discord: bool
+    service_worker: bool
+    miauth: bool
+
+
+class OptionalMeta(TypedDict, total=False):
+    pinned_page: List[str]
+    cache_remote_files: bool
+    proxy_remote_files: bool
+    require_setup: bool
+    features: Features
+
+
+class Meta(OptionalMeta):
+    maintainer_name: str
+    maintainer_email: str
+    version: str
+    name: str
+    uri: str
+    description: str
+    langs: List[str]
+    tos_url: Optional[str]
+    repository_url: str
+    feedback_url: str
+    secure: bool
+    disable_registration: bool
+    disable_local_timeline: bool
+    disable_global_timeline: bool
+    drive_capacity_per_local_user_mb: int
+    drive_capacity_per_remote_user_mb: int
+    email_required_for_signup: bool
+    enable_hcaptcha: bool
+    enable_recaptcha: bool
+    recaptcha_site_key: str
+    sw_publickey: str
+    mascot_image_url: str
+    error_image_url: str
+    max_note_text_length: int
+    emojis: List[Emoji]
+    ads: list
+    enable_email: bool
+    enable_twitter_integration: bool
+    enable_github_integration: bool
+    enable_discord_integration: bool
+    enable_service_worker: bool
+    translator_available: bool
+
+
+class OptionalInstance(TypedDict, total=False):
+    host: str
+    software_name: str
+    software_version: str
+    icon_url: str
+    favicon_url: str
+    theme_color: str
+
+
+class Instance(OptionalInstance, Meta):
+    meta: Meta
