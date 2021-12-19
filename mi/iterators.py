d074c4ae74cdc43fab7eb1aee30268f73c6cb4e5
yupix <yupi0982@outlook.jp>
2021-12-19 15:26:32 +0900

    chore: get_usersでapi関数を使わないように修正

--- mi/iterators.py
+++ mi/iterators.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
-from typing import Iterator, Optional, TYPE_CHECKING
+from typing import AsyncIterator, Optional, TYPE_CHECKING
+
+from mi.http import Route
 
 from . import User
-from .utils import api, remove_dict_empty
+from .utils import remove_dict_empty
 
 if TYPE_CHECKING:
     from . import ConnectionState
@@ -16,16 +18,16 @@ class InstanceIterator:
     def __init__(self, state: ConnectionState):
         self._state = state
 
-    def get_users(self, limit: int = 10,
-                  *,
-                  offset: int = 0,
-                  sort: Optional[str] = None,
-                  state: str = 'all',
-                  origin: str = 'local',
-                  username: Optional[str] = None,
-                  hostname: Optional[str] = None,
-                  get_all: bool = False
-                  ) -> Iterator[User]:
+    async def get_users(self, limit: int = 10,
+                        *,
+                        offset: int = 0,
+                        sort: Optional[str] = None,
+                        state: str = 'all',
+                        origin: str = 'local',
+                        username: Optional[str] = None,
+                        hostname: Optional[str] = None,
+                        get_all: bool = False
+                        ) -> AsyncIterator[User]:
         """
 
         Parameters
@@ -51,15 +53,14 @@ class InstanceIterator:
                                   'username': username,
                                   'hostname': hostname
                                   })
-        res = api('/api/admin/show-users', json_data=args, auth=True).json()
+        res = await self._state.http.request(Route('POST', '/api/admin/show-users'), json=args, auth=True, lower=True)
 
         if get_all:
             while True:
                 for i in res:
                     yield User(i, self._state)
                 args['offset'] = args['offset'] + len(res)
-                res = api('/api/admin/show-users',
-                          json_data=args, auth=True).json()
+                res = await self._state.http.request(Route('POST', '/api/admin/show-users'), json=args, auth=True, lower=True)
                 if len(res) == 0:
                     break
         else:
