7063abe0c3dc597d57006597d7c0984605948182
yupix <yupi0982@outlook.jp>
2021-12-22 02:53:14 +0900

    chore: Emojiクラスの型を正確に

--- mi/emoji.py
+++ mi/emoji.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from typing import List, TYPE_CHECKING
+from typing import List, TYPE_CHECKING, Optional
+from mi.exception import NotExistRequiredData
 
 from mi.types.emoji import Emoji as EmojiPayload
 
@@ -11,13 +12,15 @@ __all__ = ('Emoji',)
 
 class Emoji:
     def __init__(self, data: EmojiPayload, state: ConnectionState):
-        self.id: str = data.get('id')
-        self.aliases: List[str] = data.get('aliases')
-        self.name: str = data.get('name')
-        self.category: str = data.get('category')
-        self.host: str = data.get('host')
-        self.url: str = data.get('url')
+        self.id: Optional[str] = data.get('id')
+        self.aliases: Optional[List[str]] = data.get('aliases')
+        self.name: Optional[str] = data.get('name')
+        self.category: Optional[str] = data.get('category')
+        self.host: Optional[str] = data.get('host')
+        self.url: Optional[str] = data.get('url')
         self._state: ConnectionState = state
 
     async def remove(self) -> bool:
+        if not self.id:
+            raise NotExistRequiredData('idが不足しています')
         return await self._state.remove_emoji(self.id)
