f993721b60b9049ebe78b104e2e1ab971b1554bf
yupix <yupi0982@outlook.jp>
2021-12-22 22:50:10 +0900

    docs: ドキュメントのビルドにもmi.pyの依存関係を含むように

--- docs/doc_builder.sh
+++ docs/doc_builder.sh
@@ -7,9 +7,7 @@ shopt -s dotglob
 support_language=('en')
 for language in ${support_language[@]}
 do
-    export BUILDDIR=./_build/html/${language}
-    sphinx-intl update -p _build/gettext -l ${language}
-    make -e SPHINXOPTS="-D language=\'${language}\'" -b html
-    mv ./_build/html/${language}/html/* ./_build/html/${language}/
+    make -e SPHINXOPTS="-D language='en'" -e BUILDDIR="./_build/html/en" html
+    cp -r ./_build/html/${language}/html/* ./_build/html/${language}/
 done
 
