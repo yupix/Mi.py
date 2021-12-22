94eeac9981e35f3b532412c5f605d9d0c3720e8c
yupix <yupi0982@outlook.jp>
2021-12-22 21:12:16 +0900

    ci: 隠しファイルをmvで移動できるように

--- docs/doc_builder.sh
+++ docs/doc_builder.sh
@@ -2,12 +2,14 @@
 
 make html
 
+shopt -s dotglob
+
 support_language=('en')
 for language in ${support_language[@]}
 do
     export BUILDDIR=./_build/html/${language}
     sphinx-intl update -p _build/gettext -l ${language}
     make -e SPHINXOPTS="-D language=\'${language}\'" -b html
-    mv ./_build/html/${language}/html/* .[^\.]* ./_build/html/${language}/
+    mv ./_build/html/${language}/html/* ./_build/html/${language}/
 done
 
