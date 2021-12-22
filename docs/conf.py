fc8aa70c2e8afb87c7a530243ef36ca0df043767
yupix <yupi0982@outlook.jp>
2021-12-22 17:25:13 +0900

    chore: 不要なファイルを削除

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,10 @@ templates_path = ["_templates"]
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = "ja"
+language = None
+
+locale_dirs = ['locale/']
+gettext_compact = False
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
