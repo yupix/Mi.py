4700c86232e241af107046c1a65bb89eec6bea2f
yupix <yupi0982@outlook.jp>
2021-12-22 21:06:01 +0900

    ci: ビルドしたあとに言語直下に移動するように

--- docs/doc_builder.sh
+++ docs/doc_builder.sh
@@ -8,5 +8,6 @@ do
     export BUILDDIR=./_build/html/${language}
     sphinx-intl update -p _build/gettext -l ${language}
     make -e SPHINXOPTS="-D language=\'${language}\'" -b html
+    mv ./_build/html/${language}/html/* .[^\.]* ./_build/html/${language}/
 done
 
