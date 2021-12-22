99a0af00e06c4997268a194d712e29d877a7615e
yupix <yupi0982@outlook.jp>
2021-12-22 20:47:24 +0900

    feat: ドキュメントの多言語化にともなうciの変更

---
+++ docs/doc_builder.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+make html
+
+support_language=('en')
+for language in ${support_language[@]}
+do
+    export BUILDDIR=./_build/html/${language}
+    sphinx-intl update -p _build/gettext -l ${language}
+    make -e SPHINXOPTS="-D language=\'${language}\'" -b html
+done
+
