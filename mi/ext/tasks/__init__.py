582d85eca190eb0dae8965d8970bdfa6a15248da
yupix <yupi0982@outlook.jp>
2022-01-03 22:13:27 +0900

    feat: resolved MP-23 loop関数で引数の合計時間を秒に変えて待つように

--- mi/ext/tasks/__init__.py
+++ mi/ext/tasks/__init__.py
@@ -24,9 +24,11 @@ class Loop:
         self.coro: Callable[..., Coroutine[Any, Any, Any]] = coro
         self._task: Optional[asyncio.Task[Any]] = None
         self.stop_next_iteration = None
-        self._loop: Optional[asyncio.AbstractEventLoop] = custom_loop
+        self.loop: Optional[asyncio.AbstractEventLoop] = custom_loop
+        self.sleep_time = datetime.timedelta(seconds=int(seconds), minutes=int(minutes), hours=int(hours)).total_seconds()
+        self.error_coro = None
 
-    def start(self, *args: tuple[Any], **kwargs: Dict[Any, Any]) -> asyncio.Task[Any]:
+    def start(self, *args, **kwargs) -> asyncio.Task[Any]:
         """
         タスクを開始する
 
@@ -39,9 +41,9 @@ class Loop:
         -------
         _task : asyncio.Task[Any]
         """
-        
-        _loop = asyncio.get_event_loop() if self._loop is None else self._loop  # self._loop が無いなら取得
-        self._task = _loop.create_task(self.task(*args, **kwargs))
+
+        _loop = asyncio.get_event_loop() if self.loop is None else self.loop
+        self._task = _loop.create_task(self._loop(*args, **kwargs))
         return self._task
 
     def stop(self):
@@ -55,24 +57,30 @@ class Loop:
         if not self._task.done():
             self.stop_next_iteration = True
 
-    async def task(self, *args: tuple[Any], **kwargs: Dict[Any, Any]):
+    async def _loop(self, *args: tuple[Any], **kwargs: Dict[Any, Any]):
         while True:
             if self.stop_next_iteration:
                 return
-            await self.coro(*args, **kwargs)
-            await asyncio.sleep(self.seconds)
+            try:
+                await self.coro(*args, **kwargs)
+            except Exception as e:
+                await self.error_coro(e)
+            await asyncio.sleep(self.sleep_time)
+
+    def error(self, func):
+        self.error_coro = func
 
 
 def loop(
-    *,
-    seconds: float = MISSING,
-    minutes: float = MISSING,
-    hours: float = MISSING,
-    time: Union[datetime.time, Sequence[datetime.time]] = MISSING,
-    count: Optional[int] = None,
-    custom_loop: Optional[asyncio.AbstractEventLoop] = None
+        *,
+        seconds: float = MISSING,
+        minutes: float = MISSING,
+        hours: float = MISSING,
+        time: Union[datetime.time, Sequence[datetime.time]] = MISSING,
+        count: Optional[int] = None,
+        custom_loop: Optional[asyncio.AbstractEventLoop] = None
 ):
-    def _deco(func: Callable[..., Coroutine[Any, Any, Any]]) -> Loop:
+    def decorator(func):
         return Loop(
             func,
             seconds,
@@ -82,4 +90,5 @@ def loop(
             count,
             custom_loop=custom_loop
         )
-    return _deco
+
+    return decorator
