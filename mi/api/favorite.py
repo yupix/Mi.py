4dade9cd5258c93a078b000aa27139708ecf223e
yupix <yupi0982@outlook.jp>
2022-01-17 11:24:04 +0900

    fix: FavoriteManagerのメソッドでnote_idをOptionalに

--- mi/api/favorite.py
+++ mi/api/favorite.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import asyncio
-from typing import TYPE_CHECKING
+from typing import Optional, TYPE_CHECKING
 
 from mi.http import HTTPClient, Route
 
@@ -10,15 +10,19 @@ if TYPE_CHECKING:
 
 
 class FavoriteManager:
-    def __init__(self, client: 'ConnectionState', http: HTTPClient, loop: asyncio.AbstractEventLoop):
+    def __init__(self, client: 'ConnectionState', http: HTTPClient, loop: asyncio.AbstractEventLoop,
+                 note_id: Optional[str] = None):
         self.client: 'ConnectionState' = client
         self.http: HTTPClient = http
         self.loop: asyncio.AbstractEventLoop = loop
+        self.__note_id = note_id
 
-    async def add(self, note_id: str) -> bool:
+    async def add(self, note_id: Optional[str] = None) -> bool:
+        note_id = note_id or self.__note_id
         data = {'noteId': note_id}
         return bool(await self.http.request(Route('POST', '/api/notes/favorites/create'), json=data, auth=True))
 
-    async def remove(self, note_id: str) -> bool:
+    async def remove(self, note_id: Optional[str] = None) -> bool:
+        note_id = note_id or self.__note_id
         data = {'noteId': note_id}
         return bool(await self.http.request(Route('POST', '/api/notes/favorites/delete'), json=data, auth=True))
