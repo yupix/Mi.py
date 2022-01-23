ea12637e673fd44953b54fd45a367c58f9552313
yupix <yupi0982@outlook.jp>
2022-01-23 20:50:28 +0900

    feat: UserActionsクラスにchat属性を追加

--- mi/actions/user.py
+++ mi/actions/user.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import asyncio
 from typing import Optional, TYPE_CHECKING
 
+from mi.api.chat import ChatManager
 from mi.api.follow import FollowManager, FollowRequestManager
 
 if TYPE_CHECKING:
@@ -23,8 +24,9 @@ class UserActions:
         self.client = client
         self.http = http
         self.loop = loop
-        self.follow = FollowManager(client, http, loop, user_id=user_id)
-        self.follow_request = FollowRequestManager(client, http, loop, user_id=user_id)
+        self.follow: FollowManager = FollowManager(client, http, loop, user_id=user_id)
+        self.follow_request: FollowRequestManager = FollowRequestManager(client, http, loop, user_id=user_id)
+        self.chat: ChatManager = ChatManager(client, http, loop, user_id=user_id)
 
     def get_follow(self, user_id: str) -> FollowManager:
         return FollowManager(self.client, self.http, self.loop, user_id=user_id)
