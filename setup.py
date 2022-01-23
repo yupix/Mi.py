1f2eb207c40f51c2e17f4e20fbf83cda66e0e40a
yupix <yupi0982@outlook.jp>
2022-01-23 21:41:48 +0900

    fix: instanceがimportできていなかった

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ packages = [
 
 setup(
     name="mi.py",
-    version="3.0.0",
+    version="3.0.1",
     install_requires=["emoji", 'aiocache', 'aiohttp'],
     url="https://github.com/yupix/mi.py",
     author="yupix",
