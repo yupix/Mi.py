967a391aeef61f1cb9a3420257e34c71c8b8e309
yupix <yupi0982@outlook.jp>
2022-01-25 20:43:21 +0900

    feat: add get_files method

--- mi/api/drive.py
+++ mi/api/drive.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 import asyncio
-from typing import Optional, TYPE_CHECKING
+from typing import List, Optional, TYPE_CHECKING
 
 from mi.drive import File, Folder
+from mi.exception import InvalidParameters
 from mi.http import HTTPClient, Route
 from mi.models.drive import RawFile, RawFolder
 from mi.utils import remove_dict_empty
@@ -63,6 +64,16 @@ class FolderManager:
         data = {'folderId': folder_id}
         return bool(await self.__http.request(Route('POST', '/api/drive/folders/delete'), json=data, lower=True, auth=True))
 
+    async def get_files(self, limit: int = 10, since_id: Optional[str] = None, until_id: Optional[str] = None, folder_id: Optional[str] = None, file_type: Optional[str] = None) -> List[File]:
+
+        if limit >= 100:
+            raise InvalidParameters('limit must be less than 100')
+
+        folder_id = folder_id or self.__folder_id
+        data = {'limit': limit, 'sinceId': since_id, 'untilId': until_id, 'folderId': folder_id, 'Type': file_type}
+        res = await self.__http.request(Route('POST', '/api/drive/files'), json=data, auth=True, lower=True)
+        return [File(RawFile(i), state=self.__state) for i in res]
+
 
 class DriveManager:
     def __init__(self, __state: 'ConnectionState', http: HTTPClient, loop: asyncio.AbstractEventLoop):
@@ -71,7 +82,7 @@ class DriveManager:
         self.__loop = loop
 
     async def get_folders(self, limit: int = 100, since_id: Optional[str] = None, until_id: Optional[str] = None,
-                          folder_id: Optional[str] = None):
+                          folder_id: Optional[str] = None) -> List[Folder]:
         data = {
             'limit': limit,
             'sinceId': since_id,
