0f05fe2d603c02b017896a63c104ceae6e60fc26
yupix <yupi0982@outlook.jp>
2022-01-25 23:25:24 +0900

    chore: docstringの追加

--- mi/models/chat.py
+++ mi/models/chat.py
@@ -5,7 +5,40 @@ from .user import RawUser
 
 
 class RawChat:
+    """
+    Attributes
+    ----------
+    id : str
+        chat ID
+    created_at : str
+        chat creation time
+    content : str
+        chat content
+    user_id : str
+       ID of the user who created the chat
+    author : RawUser
+        user who created the chat
+    recipient_id : str
+    recipient : str
+    group_id : str
+        group ID
+    file_id : str
+        ID of the attached file
+    is_read : bool
+        whether the chat is read
+    reads : List
+    """
+
+    __slots__ = ('id', 'created_at', 'content', 'user_id', 'author', 'recipient_id', 'recipient', 'group_id', 'file_id',
+                 'is_read', 'reads')
+
     def __init__(self, data: ChatPayload):
+        """
+
+        Parameters
+        ----------
+        data: ChatPayload
+        """
         self.id: str = data["id"]
         self.created_at: str = data["created_at"]
         self.content: str = data["text"]
@@ -15,5 +48,5 @@ class RawChat:
         self.recipient: str = data["recipient"]
         self.group_id: str = data["group_id"]
         self.file_id: str = data["file_id"]
-        self.is_read: bool = data["is_read"]
+        self.is_read: bool = bool(data["is_read"])
         self.reads: List = data["reads"]
