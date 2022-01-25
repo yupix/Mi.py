03939f4cda3af9fece61658b0d500027ab79b975
yupix <yupi0982@outlook.jp>
2022-01-25 20:45:49 +0900

    feat: bump version

--- mi/__init__.py
+++ mi/__init__.py
@@ -4,7 +4,7 @@ __license__ = "MIT"
 __copyright__ = "Copyright 2021-present yupix"
 __author_email__ = "yupi0982@outlook.jp"
 __url__ = "https://github.com/yupix/Mi.py"
-__version__ = "3.2.1"
+__version__ = "3.3.0"
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
