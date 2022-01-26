c89db6f4d1a786f4e15a148241714e7e704d8bd2
yupix <yupi0982@outlook.jp>
2022-01-27 00:45:13 +0900

    feat: UserActionsにget_notesメソッドを追加

--- mi/actions/user.py
+++ mi/actions/user.py
@@ -1,15 +1,18 @@
 from __future__ import annotations
 
 import asyncio
-from typing import Optional, TYPE_CHECKING
+from typing import List, Optional, TYPE_CHECKING
 
 from mi.api.chat import ChatManager
 from mi.api.follow import FollowManager, FollowRequestManager
+from mi.api.note import NoteManager
 from mi.exception import NotExistRequiredData
+from mi.http import HTTPClient, Route
+from mi.models.note import RawNote
+from mi.note import Note
 
 if TYPE_CHECKING:
     from mi.state import ConnectionState
-    from mi.http import HTTPClient
     from mi.user import User
 
 __all__ = ['UserActions']
@@ -28,9 +31,41 @@ class UserActions:
         self.__http = http
         self.__loop = loop
         self.__user: User = user
+        self.note: NoteManager(state, http, loop, user_id=user_id)
         self.follow: FollowManager = FollowManager(state, http, loop, user_id=user_id)
         self.follow_request: FollowRequestManager = FollowRequestManager(state, http, loop, user_id=user_id)
         self.chat: ChatManager = ChatManager(state, http, loop, user_id=user_id)
+
+    async def get_notes(
+            self,
+            user_id: Optional[str] = None,
+            include_replies: bool = True,
+            limit: int = 10,
+            since_id: Optional[str] = None,
+            until_id: Optional[str] = None,
+            since_date: int = 0,
+            until_date: int = 0,
+            include_my_renotes: bool = True,
+            with_files: bool = False,
+            file_type: Optional[List[str]] = None,
+            exclude_nsfw: bool = True
+    ) -> List[Note]:
+        user_id = user_id or self.__user.id
+        data = {
+            'userId': user_id,
+            'includeReplies': include_replies,
+            'limit': limit,
+            'sinceId': since_id,
+            'untilId': until_id,
+            'sinceDate': since_date,
+            'untilDate': until_date,
+            'includeMyRenotes': include_my_renotes,
+            'withFiles': with_files,
+            'fileType': file_type,
+            'excludeNsfw': exclude_nsfw
+        }
+        res = await self.__http.request(Route('POST', '/api/users/notes'), json=data, auth=True, lower=True)
+        return [Note(RawNote(i), state=self.__state) for i in res]
 
     def get_mention(self, user: Optional[User] = None) -> str:
         user = user or self.__user
