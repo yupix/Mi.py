a6a1f0c5f8c5f5f3c3af20c39068f4d528baf2e6
yupix <yupi0982@outlook.jp>
2022-02-07 18:28:22 +0900

    feat: AdminUserManagerにメソッドを追加

--- mi/wrapper/user.py
+++ mi/wrapper/user.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from mi import config
 from mi.exception import NotSupportedError
 from mi.framework.http import HTTPSession, Route
@@ -6,8 +8,8 @@ from mi.wrapper import RawUser
 
 
 class AdminUserManager:
-    def __init__(self):
-        pass
+    def __init__(self, user_id: Optional[str] = None):
+        self.__user_id = user_id
 
     @staticmethod
     async def create_account(username: str, password: str) -> User:
@@ -17,8 +19,27 @@ class AdminUserManager:
         res = await HTTPSession.request(Route('POST', '/api/admin/accounts/create'), json=data, auth=True, lower=True)
         return User(RawUser(res))
 
-    @staticmethod
-    async def delete_account(user_id: str) -> bool:
+    async def delete_account(self, user_id: Optional[str] = None) -> bool:
+        user_id = user_id or self.__user_id
+
         data = {'userId': user_id}
         res = await HTTPSession.request(Route('POST', '/api/admin/accounts/delete'), json=data, auth=True, lower=True)
+        return bool(res)
+
+    async def show(self, user_id: Optional[str] = None) -> User:
+        user_id = user_id or self.__user_id
+        data = {'userId': user_id}
+        res = await HTTPSession.request(Route('GET', '/api/admin/show-user'), json=data, auth=True, lower=True)
+        return User(RawUser(res))
+
+    async def suspend(self, user_id: Optional[str] = None) -> bool:
+        user_id = user_id or self.__user_id
+        data = {'userId': user_id}
+        res = await HTTPSession.request(Route('POST', '/api/admin/suspend-user'), json=data, auth=True, lower=True)
+        return bool(res)
+
+    async def unsuspend(self, user_id: Optional[str] = None) -> bool:
+        user_id = user_id or self.__user_id
+        data = {'userId': user_id}
+        res = await HTTPSession.request(Route('POST', '/api/admin/unsuspend-user'), json=data, auth=True, lower=True)
         return bool(res)
