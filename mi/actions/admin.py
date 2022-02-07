d6441a508748b8b54cf640d2516906d91835c554
yupix <yupi0982@outlook.jp>
2022-02-07 17:22:05 +0900

    feat: AdminModeratorManagerクラスを追加

--- mi/actions/admin.py
+++ mi/actions/admin.py
@@ -1,7 +1,9 @@
 from typing import Optional
 
+from mi.framework.http import HTTPSession, Route
 from mi.wrapper.ad import AdminAdvertisingManager
 from mi.wrapper.emoji import AdminEmojiManager
+from mi.wrapper.moderator import AdminModeratorManager
 from mi.wrapper.user import AdminUserManager
 
 
@@ -10,7 +12,12 @@ class AdminActions:
         self.emoji = AdminEmojiManager()
         self.user = AdminUserManager()
         self.ad = AdminAdvertisingManager()
+        self.moderator = AdminModeratorManager()
 
     @staticmethod
     def get_emoji_instance(emoji_id: Optional[str] = None) -> AdminEmojiManager:
         return AdminEmojiManager(emoji_id)
+
+    @staticmethod
+    async def get_invite() -> bool:
+        return bool(await HTTPSession.request(Route('POST', '/api/admin/invite')))
