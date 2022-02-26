312289b10230d20ffdb661379cde6760f5b7c33a
yupix <yupi0982@outlook.jp>
2022-02-26 23:04:11 +0900

    feat: DocStringを追加 issue #158

--- mi/wrapper/user.py
+++ mi/wrapper/user.py
@@ -1,3 +1,5 @@
+"""Modules for the administrator's users"""
+
 from typing import Optional
 
 from mi import config
@@ -13,6 +15,22 @@ class AdminUserManager:
 
     @staticmethod
     async def create_account(username: str, password: str) -> User:
+        """
+        Create a new account.
+
+        Parameters
+        ----------
+        username : str
+            User's name
+        password : str
+            User's password
+
+        Returns
+        -------
+        User
+            Created user
+        """
+
         if config.is_ayuskey:
             raise NotSupportedError("Ayuskeyではサポートされていません")
         data = {'username': username, 'password': password}
@@ -20,25 +38,79 @@ class AdminUserManager:
         return User(RawUser(res))
 
     async def delete_account(self, user_id: Optional[str] = None) -> bool:
+        """
+        Deletes the user with the specified user ID.
+
+        Parameters
+        ----------
+        user_id : Optional[str], default=None
+            ID of the user to be deleted
+        Returns
+        -------
+        bool
+            Success or failure
+        """
+
         user_id = user_id or self.__user_id
 
         data = {'userId': user_id}
         res = await HTTPSession.request(Route('POST', '/api/admin/accounts/delete'), json=data, auth=True, lower=True)
         return bool(res)
 
-    async def show(self, user_id: Optional[str] = None) -> User:
+    async def show_user(self, user_id: Optional[str] = None) -> User:
+        """
+        Shows the user with the specified user ID.
+
+        Parameters
+        ----------
+        user_id : Optional[str], default=None
+            ID of the user to be shown
+
+        Returns
+        -------
+        User
+        """
+
         user_id = user_id or self.__user_id
         data = {'userId': user_id}
         res = await HTTPSession.request(Route('GET', '/api/admin/show-user'), json=data, auth=True, lower=True)
         return User(RawUser(res))
 
     async def suspend(self, user_id: Optional[str] = None) -> bool:
+        """
+        Suspends the user with the specified user ID.
+
+        Parameters
+        ----------
+        user_id : Optional[str], default=None
+            ID of the user to be suspended
+
+        Returns
+        -------
+        bool
+            Success or failure
+        """
+
         user_id = user_id or self.__user_id
         data = {'userId': user_id}
         res = await HTTPSession.request(Route('POST', '/api/admin/suspend-user'), json=data, auth=True, lower=True)
         return bool(res)
 
     async def unsuspend(self, user_id: Optional[str] = None) -> bool:
+        """
+        Unsuspends the user with the specified user ID.
+
+        Parameters
+        ----------
+        user_id : Optional[str], default=None
+            ID of the user to be unsuspended
+
+        Returns
+        -------
+        bool
+            Success or failure
+        """
+
         user_id = user_id or self.__user_id
         data = {'userId': user_id}
         res = await HTTPSession.request(Route('POST', '/api/admin/unsuspend-user'), json=data, auth=True, lower=True)
