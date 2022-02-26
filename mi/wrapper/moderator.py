ad8a4752c63166ce91b56b6ef08f5f2c86d66e07
yupix <yupi0982@outlook.jp>
2022-02-26 21:14:52 +0900

    feat: DocStringを追加

--- mi/wrapper/moderator.py
+++ mi/wrapper/moderator.py
@@ -8,12 +8,39 @@ class AdminModeratorManager:
         self.__user_id: Optional[str] = user_id
 
     async def add(self, user_id: Optional[str] = None) -> bool:
+        """
+        Add a user as a moderator
+
+        Parameters
+        ----------
+        user_id : Optional[str], default=None
+            ユーザーのID
+
+        Returns
+        -------
+        bool
+            成功したか否か
+        """
+
         user_id = user_id or self.__user_id
         data = {'userId': user_id}
         res = await HTTPSession.request(Route('POST', '/api/moderators/add'), json=data, auth=True, lower=True)
         return bool(res)
 
     async def remove(self, user_id: Optional[str] = None) -> bool:
+        """
+        Unmoderate a user
+
+        Parameters
+        ----------
+        user_id : Optional[str], default=None
+            ユーザーのID
+
+        Returns
+        -------
+        bool
+            成功したか否か
+        """
         user_id = user_id or self.__user_id
         data = {'userId': user_id}
         res = await HTTPSession.request(Route('POST', '/api/moderators/remove'), json=data, auth=True, lower=True)
